5a09f0917993c0d8d4d2537dbd3f2786850d6162
Yash Agrawal <yashagrawal01meh@gmail.com>
2021-12-08 19:27:27 +0530

    Request URL Bug

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,7 @@ const listPoducts = () => {
   return async (dispatch) => {
     try {
       dispatch({ type: "PRODUCT_LIST_Request" });
-      const { data } = await axios.get("http://127.0.0.1:5000/api/products");
+      const { data } = await axios.get("/api/products");
 
       dispatch({ type: "PRODUCT_LIST_SUCCESS", payload: data });
     } catch (error) {
