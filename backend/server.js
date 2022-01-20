8a06e60db36ecc8ecac3a8b70c90e6d2b6c6a5d9
Yash Agrawal <yashagrawal01meh@gmail.com>
2022-01-20 11:21:50 +0530

    heroku bug

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import productRoute from "./routes/productRoute";
 import orderRouter from "./routes/orderRouter";
 import categoryRouter from "./routes/categoryRouter";
 
-// const cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary").v2;
 
 dotenv.config();
 
@@ -21,11 +21,12 @@ mongoose
   })
   .catch((error) => console.log(error));
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // console.log(cloudinary);
 
@@ -34,20 +35,20 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-// app.use("/api/uploads", async (req, res) => {
-//   // console.log(req.body.data);
-//   try {
-//     const fileStr = req.body.data;
-//     const uploadResponse = await cloudinary.uploader.upload(fileStr, {
-//       upload_preset: "qzbic9ps",
-//     });
-//     // console.log(uploadResponse);
-//     res.status(200).send({ msg: "image uploaded", data: uploadResponse });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ err: "Something went wrong" });
-//   }
-// });
+app.use("/api/uploads", async (req, res) => {
+  // console.log(req.body.data);
+  try {
+    const fileStr = req.body.data;
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "qzbic9ps",
+    });
+    // console.log(uploadResponse);
+    res.status(200).send({ msg: "image uploaded", data: uploadResponse });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRouter);
@@ -61,12 +62,12 @@ app.use("/api/category", categoryRouter);
 //   );
 //   next();
 // });
-// const __dirname = path.resolve();
-// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-// app.use(express.static(path.join(__dirname, "/frontend/build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
-// });
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+});
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("build"));
